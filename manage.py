0132679a587f99f0b0377b8228d31f9d851725e1
Ubuntu <ubuntu@ip-172-31-47-117.ec2.internal>
2025-01-13 11:04:12 +0000

    Second commit

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's Hii Abhishek command-line utility for administrative tasks."""
+"""Hello Django's Hii Abhishek command-line utility for administrative tasks."""
 import os
 import sys
 
